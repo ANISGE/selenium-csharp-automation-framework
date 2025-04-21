af8a95a1c7821539b0fe7476778bcd3a13a5e898
anishassoun <anis.hassoun@global-e.com>
2025-04-21 16:55:29 +0300

    Add GitHub Actions CI workflow

--- obj/Debug/net8.0/SeleniumTestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/SeleniumTestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleniumTestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+324c6628d3f47f6bbfd2d54dfd64408b638e8be0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleniumTestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeleniumTestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
